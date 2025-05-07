e9c26995edfcda33677ba9eb5fdffc41f4765d9c
Rafael Hendges Coutinho <104592522+rafaelhendges@users.noreply.github.com>
2025-05-07 19:01:05 -0300

    Add files via upload

---
+++ .js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("formulario");
+  
+    form.addEventListener("submit", () => {
+      alert("Obrigado! Seu orçamento foi solicitado com sucesso.");
+    });
+  });
+  
